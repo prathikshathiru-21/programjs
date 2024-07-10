3ab59f316cf4e8515523eb1e2d7651aa5b823d79
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-10 14:55:21 -0500

    completed

--- 03_JS_Objects/Ch3_pgm_03.js
+++ 03_JS_Objects/Ch3_pgm_03.js
@@ -3,13 +3,19 @@
 var book;
 
 book = {
-    title : "The Hobbit",
+    title : "The Hobbit, or There and Back Again",
     author : "J. R. R. Tolkien",
     published : 1937
 };
 
 console.log(book);
 
+var book2 = {
+    title: "1984",
+    author: "Georgr Orwell",
+    published: 1949
+};
+console.log(book2)
 
 
 /* Further Adventures
