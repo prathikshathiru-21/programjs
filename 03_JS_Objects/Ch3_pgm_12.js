8cfd4dbe18e0cf34a65f4139e249913fb71b7bcb
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-10 15:45:12 -0500

    completed

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -9,6 +9,25 @@ var post = {
 };
 
 
+console.log("Post Id:" + post.id);
+console.log("Title" + post.title);
+console.log("Author" + post.author);
+console.log("Created" + post.created);
+console.log("Body:\n" + post.body);
+
+var post2 = {
+    id: 2,
+    title: "Exploring the Deep Sea",
+    author: "Abyss",
+    created: "2018-09-15",
+    body: "Diving into the abyssal depths, discovering new species never seen before..."
+};
+
+console.log("\nPost 2 ID: " + post2.id);
+console.log("Title: " + post2.title);
+console.log("Author: " + post2.author);
+console.log("Created Date: " + post2.created);
+console.log("Body:\n" + post2.body);
 
 /* Further Adventures
  *
