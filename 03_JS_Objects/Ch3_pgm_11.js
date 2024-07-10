f8e2198aea54761c2269f019eb1589fe79438b94
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-10 15:39:03 -0500

    completed

--- 03_JS_Objects/Ch3_pgm_11.js
+++ 03_JS_Objects/Ch3_pgm_11.js
@@ -13,7 +13,22 @@ player1.score = player1.score + 50;
 
 console.log(player1.name + " has scored " + player1.score);
 
+player1.score = player1.score * 1.1;
 
+console.log(player1.name + "'s score after 10% increase: " + player1.score);
+
+player2 = {
+    name: "Alice",
+    score: 0
+};
+player1.score = player1.score + 50; 
+player2.score = player2.score + 75; 
+
+console.log(player1.name + " has scored " + player1.score);
+console.log(player2.name + " has scored " + player2.score);
+
+var totalScore = player1.score + player2.score;
+console.log("Total score of " + player1.name + " and " + player2.name + " is " + totalScore);
 
 /* Further Adventures
  *
