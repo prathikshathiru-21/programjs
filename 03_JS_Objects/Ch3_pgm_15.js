e6fcc972a987d51e64281997481c075fa420216e
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-10 15:50:15 -0500

    completed

--- 03_JS_Objects/Ch3_pgm_15.js
+++ 03_JS_Objects/Ch3_pgm_15.js
@@ -9,10 +9,22 @@ player = {
     items: "a rusty key, The Sword of Destiny, a piece of cheese"
 };
 
+var player2 = {
+    name: "Thorne",
+    health: 70,
+    place: "The Enchanted Forest",
+    items: ["a magic potion", "a bow", "a map"]
+};
+
 console.log(player.name);
 console.log(player.name + " is in " + player.place);
 console.log(player.name + " has health " + player.health);
 console.log("Items: " + player.items);
+
+console.log("\n" + player2.name);
+console.log(player2.name + " is in " + player2.place);
+console.log(player2.name + " has health " + player2.health);
+console.log("Items: " + player2.items.join(", "));
 
 
 
