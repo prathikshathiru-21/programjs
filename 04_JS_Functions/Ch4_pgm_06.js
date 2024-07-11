a5e8fd71f90f3e3345aebf38a5344dfd043c9d98
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-11 14:31:23 -0500

    completed

--- 04_JS_Functions/Ch4_pgm_06.js
+++ 04_JS_Functions/Ch4_pgm_06.js
@@ -3,14 +3,22 @@
 var sayHello;
 
 sayHello = function () {
-	console.log("Hello World!");
+	console.log("Hello \nWorld!");
 };
 
 sayHello();
 sayHello();
 sayHello();
 
+var printletters;
+printletters = function () {
+	var message ="Hello World!";
+	for (var i = 0; i < message.length; i++){
+		console.log(message[i]);
+	}
+}
 
+printletters();
 
 /* Further Adventures
  *
