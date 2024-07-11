6142abb980f14f1b7bcdd4a433d21ad07a9af02c
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-11 15:24:50 -0500

    completed

--- 05_JS_Arguments/Ch5_pgm_07.js
+++ 05_JS_Arguments/Ch5_pgm_07.js
@@ -3,7 +3,8 @@
 var showPlayerName;
 
 showPlayerName = function (playerName) {
-    console.log(playerName);
+    console.log("The player's name is " + playerName);
+    console.log("The name has " + playerName.length + " letters.");
 };
 
 showPlayerName("Kandra");
