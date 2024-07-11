1842d6d7e6ed30b684713008deb46ea4e26e5548
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-11 15:18:55 -0500

    completed

--- 05_JS_Arguments/Ch5_pgm_05.js
+++ 05_JS_Arguments/Ch5_pgm_05.js
@@ -13,7 +13,28 @@ square(-2);
 square(1111);
 square(0.5);
 
+var cube = function (numberToCube) {
+  var result;
+  result = numberToCube * numberToCube * numberToCube;
+  console.log(numberToCube + " * " + numberToCube + " * " + numberToCube + " = " + result);
+};
 
+
+cube(3);
+cube(5);
+cube(-2);
+cube(0.1);
+
+var squareRoot = function (number) {
+  var result = Math.sqrt(number);
+  console.log("The square root of " + number + " is " + result);
+};
+
+
+squareRoot(9);
+squareRoot(16);
+squareRoot(25);
+squareRoot(2);
 
 /* Further Adventures
  *
