2de99c4100e5a9b4bd7218a2fb8dba08b9e5258a
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-12 15:16:20 -0500

    completed

--- 08_JS_Arrays/Ch8_pgm_01.js
+++ 08_JS_Arrays/Ch8_pgm_01.js
@@ -2,14 +2,24 @@
 
 var scores;
 var names;
+var akaScores;
 
-scores = [ 3, 1, 8, 2 ];
-names = [ "Kandra", "Dax", "Blinky" ];
+scores = [ 3, 1, 8, 2];
+names = [ "Kandra", "Dax", "Blinky"];
 
 console.log(scores);
 console.log(names);
 
+scores.push(5);
+names.push("Zelda");
 
+console.log(scores);
+console.log(names);
+
+akaScores = scores;
+
+akaScores = scores;
+console.log(akaScores);
 
 /* Further Adventures
  *
