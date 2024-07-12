a1ef613b506cbe0860233dfa9a75a31c81655657
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-12 15:25:09 -0500

    completed

--- 08_JS_Arrays/Ch8_pgm_03.js
+++ 08_JS_Arrays/Ch8_pgm_03.js
@@ -1,13 +1,19 @@
 // Accessing array elements
 
-var scores = [ 3, 1, 8, 2 ];
+var scores = [ 3, 1, 8, 2];
+
+scores.push(15);
 
 console.log("There are " + scores.length + " scores:");
 console.log("The first score is " + scores[0]); 
 console.log("The second score is " + scores[1]);
 console.log("The third score is " + scores[2]);
 console.log("The fourth score is " + scores[3]);
-
+console.log("The fifth score is " + scores[4]);
+console.log("The last score is " + scores[scores.length - 1]);
+scores.push(30);
+console.log("The sixth score is " + scores[5]);
+console.log("The last score is " + scores[scores.length - 1]);
 
 
 /* Further Adventures
