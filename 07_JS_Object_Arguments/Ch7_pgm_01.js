a6dd8f57e8dfeae86b472d717e758490295dfe50
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-11 19:30:24 -0500

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_01.js
+++ 07_JS_Object_Arguments/Ch7_pgm_01.js
@@ -1,6 +1,7 @@
 // Passing a function an object as an argument
 
 var planet1;
+var planet2;
 var getPlanetInfo;
 
 planet1 = {
@@ -11,11 +12,23 @@ planet1 = {
     sizeRank: 1
 };
 
+planet2 = {
+    name: "Mars",
+    position: 4,
+    type: "Terrestrial",
+    radius: 3389,
+    sizeRank: 2
+};
+
 getPlanetInfo = function (planet) {
-    return planet.name + ": planet number " + planet.position;
+    return planet.name + ": planet number " + planet.position + "\n" +
+           "Type: " + planet.type + "\n" +
+           "Radius: " + planet.radius + " km\n" +
+           "Size Rank: " + planet.sizeRank;
 };
 
 console.log(getPlanetInfo(planet1));
+console.log(getPlanetInfo(planet2));
 
 
 
