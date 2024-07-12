1400c8a2608a299b8d425614a2c7a5ea98190b14
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-12 09:29:12 -0500

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_05.js
+++ 07_JS_Object_Arguments/Ch7_pgm_05.js
@@ -9,6 +9,31 @@ var showSmaller = function (num1, num2) {
 
 showSmaller(12, 3);
 showSmaller(-10, 3);
+showSmaller(20, 10);
+showSmaller(-30, -10);
+
+var showLarger = function (num1, num2) {
+    var smaller = Math.min(num1, num2);
+    var larger = Math.max(num1, num2);
+
+    console.log(larger + " is larger than " + smaller);
+};
+
+showLarger(12, 3);
+showLarger(-10, 3);
+showLarger(7, 7);
+showLarger(100, 0);
+
+var showSmaller = function(num1, num2 ,num3){
+  var smaller = Math.min(num1, num2, num3);
+  console.log(smaller + "is the smallest of" + num1 + "," + num2 + ", and" + num3)
+};
+
+showSmaller(12, 34, 6);
+showSmaller(122, 450, 30);
+showSmaller(100, 0, 40);
+showSmaller(1000, 15, 20);
+
 
 
 
