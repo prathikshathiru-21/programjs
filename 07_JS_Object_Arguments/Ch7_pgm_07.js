2da7ccd5840436fa19a3c767d498b1f93d6ff484
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-12 14:56:47 -0500

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_07.js
+++ 07_JS_Object_Arguments/Ch7_pgm_07.js
@@ -5,7 +5,18 @@ var bigPlanet = planet.toUpperCase();
 
 console.log(planet + " becomes " + bigPlanet);
 
+var getBig = function (str) {
+     return str.toUpperCase();
+};
 
+var getSmall = function (str) {
+   return str.toLowerCase();
+};
+
+console.log(getBig("Neptune"));
+console.log(getSmall("Mars"));
+console.log(getBig("Saturn"));
+console.log(getSmall("URANUS"));
 
 /* Further Adventures
  *
