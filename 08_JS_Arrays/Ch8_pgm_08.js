34459e88a1ce68e0da0b5cc117191ed1f321eb66
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-13 09:51:19 -0500

    completed

--- 08_JS_Arrays/Ch8_pgm_08.js
+++ 08_JS_Arrays/Ch8_pgm_08.js
@@ -4,10 +4,21 @@ var items = [ "The Pyramids", "The Grand Canyon", "Bondi Beach" ];
 
 console.log("Dream destinations:");
 
+console.log("Number of places to visit:" + items.length);
+
 items.forEach(function (item) {
 	console.log(" – " + item);
 });
 
+var showItems = function () {
+    console.log("Dream destinations:");
+    console.log("Number of places to visit: " + items.length);
+    items.forEach(function (item) {
+        console.log(" – " + item);
+    });
+};
+
+showItems();
 
 
 /* Further Adventures
