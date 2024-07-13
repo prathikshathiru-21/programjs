0c16cb0f4ba3ed353300e0ed2416042f89ba44ba
prathikshathiru-21 <prathikshathiru0@outlook.com>
2024-07-13 09:29:23 -0500

    completed

--- 08_JS_Arrays/Ch8_pgm_05.js
+++ 08_JS_Arrays/Ch8_pgm_05.js
@@ -19,11 +19,29 @@ var getVisitorReport = function (visitorArray, dayInWeek) {
 	return visitorReport;
 };
 
-var visitors = [ 354, 132, 210, 221, 481 ];
+var visitors = [ 354, 132, 210, 221, 481, 512, 376 ];
 
-var report = getVisitorReport(visitors, 2);
+var report = getVisitorReport(visitors, 5);
 
 console.log(report);
+
+var getMonthlyVisitorReport = function (monthArray, week, day) {
+  var weekVisitors = monthArray[week - 1];
+  return getVisitorReport(weekVisitors, day);
+};
+
+
+var week1 = [354, 132, 210, 221, 481, 512, 376];
+var week2 = [400, 250, 300, 180, 520, 430, 390];
+var week3 = [380, 260, 270, 310, 500, 460, 420];
+var week4 = [410, 290, 320, 230, 540, 470, 410];
+
+var month = [week1, week2, week3, week4];
+
+console.log(getMonthlyVisitorReport(month, 2, 5)); 
+console.log(getMonthlyVisitorReport(month, 1, 7)); 
+console.log(getMonthlyVisitorReport(month, 3, 3)); 
+console.log(getMonthlyVisitorReport(month, 4, 6));
 
 
 
